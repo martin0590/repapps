8ca6f883de114efb8e370fa8d5c35153d52d15bb
Martin Alejandro Mammana <martin.ale590@gmail.com>
2024-11-27 15:18:00 -0300

    chore - added card in main page

--- constants/index.ts
+++ constants/index.ts
@@ -12,6 +12,12 @@ export const links = [
     image: '/images/procons.png',
     url: '/procons',
     description: 'Crea una lista de ventajas y desventajas de lo que quieras.'
+  },{
+    id: 3,
+    title: 'Text Transform',
+    image: '/images/texttransform.png',
+    url: '/texttransform',
+    description: 'capitaliza, minúscula y mayúscula tu texto.'
   }
 ]
 
